7d5e6c2bb57d716fc9bd61296bfb6c4897be0c4f
wanghaorangithub <wanghaorangithub@users.noreply.github.com>
2018-03-24 00:41:01 +0800

    完成

--- DSQ1/DSQ1.cpp
+++ DSQ1/DSQ1.cpp
@@ -3,11 +3,12 @@
 
 #include "stdafx.h" 
 #include "arraylist.h"
-
+#include "linklist.h"
+void solveTheProblemWithArrayList(int n, int s, int m); 
+void solveTheProblemWithLinkList(int n, int s, int m);
 int main()
 {
 	int n = 0, s = 0, m = 0;
-	int position = 0,result=0;
 	std::cout << "please enter three number" << std::endl;
 	std::cout << "n:";
 	std::cin >> n;
@@ -15,6 +16,12 @@ int main()
 	std::cin >> s;
 	std::cout << "m:";
 	std::cin >> m;
+	solveTheProblemWithArrayList(n, s, m);
+	solveTheProblemWithLinkList(n, s, m);
+	return 0;
+}
+void solveTheProblemWithArrayList(int n, int s, int m) {
+	int position = 0, result = 0;
 	//initial arrayList
 	ArrayList <int> *arrayList = new ArrayList<int>(n);
 	for (int i = 1; i <= n; i++) {
@@ -23,6 +30,7 @@ int main()
 	}
 	//get result
 	position = s;
+	std::cout << "the result in ArrayList is:";
 	while (arrayList->getSize() > 0) {
 		position = (position + m - 2) % arrayList->getSize();
 		result = arrayList->get(position);
@@ -30,6 +38,25 @@ int main()
 		arrayList->deletetDataFromArrayList(position);
 		position++;
 	}
-	return 0;
+	std::cout << std::endl;
 }
-
+void solveTheProblemWithLinkList(int n, int s, int m) {
+	int position = 0, result = 0;
+	//initial linkList
+	LinkList <int> *linkList = new LinkList<int>(n);
+	for (int i = 1; i <= n; i++) {
+		position = i - 1;
+		linkList->set(i, position);
+	}
+	//get result
+	position = s;
+	std::cout << "the result in LinkList is:";
+	while (linkList->getSize() > 0) {
+		position = (position + m - 2) % linkList->getSize();
+		result = linkList->get(position);
+		std::cout <<  result << " ";
+		linkList->deletetDataFromLinkList(position);
+		position++;
+	}
+	std::cout << std::endl;
+}
---
+++ DSQ1/linklist.h
@@ -0,0 +1,92 @@
+#pragma once
+#ifndef LINKLIST
+#define LINKLIST
+#include <iostream>
+
+template<typename T>
+struct Node
+{
+	T data;
+	Node<T>*next;
+	Node(Node<T>*next = nullptr) { this->next = next; }
+	Node(const T &data, Node<T> *next = nullptr) {
+		this->data = data;
+		this->next = next;
+	}
+};
+template<typename T>
+class LinkList
+{
+private:
+	Node<T> *head;
+		int size;
+public:
+	LinkList(int size) {
+		this->size = size;
+		head = new Node<T>;
+		Node<T> *p = head;
+		for(int i = 0;i<size;i++)
+		{
+			p->next = new Node<T>(nullptr);
+			p = p->next;
+		}
+	}
+	~LinkList() {
+		Link<T>*tmp;
+		while (head != nullptr) {
+			tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+	}
+	bool deletetDataFromLinkList(int &position) {
+		if (position >= size) {
+			return false;
+		}
+		else
+		{
+			Node<T> *tmp = head;
+			Node<T> *m = nullptr, *q = nullptr;
+			for (int i = 0; i < position; i++)
+				tmp = tmp->next;
+			m = tmp->next;
+			if (m->next != nullptr) {
+				q = m->next;
+				tmp->next = q;
+			}
+			else
+				tmp->next = nullptr;
+			size--;
+			delete m;
+			return true;
+		}
+	}
+	int getSize() {
+		return size;
+	}
+	bool set(T data,int position){
+		if (position < size) {
+			Node<T> *tmp=head->next;
+			for (int i = 0; i < position; i++)
+				tmp = tmp->next;
+			tmp->data = data;
+			return true;
+		}
+		else
+			std::cout << "Over filled!" << std::endl;
+		return false;
+	}
+	T get(int position) {
+		if (position < size) {
+			Node<T> *tmp = head->next;
+			for (int i = 0; i < position; i++)
+				tmp = tmp->next;
+			return tmp->data;
+		}
+		else {
+			std::cout << "Over filled!" << std::endl;
+			return 0;
+		}
+	}
+};
+#endif // !LINKLIST
